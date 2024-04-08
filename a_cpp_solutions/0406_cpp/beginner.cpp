424f17aa3776e7da6070e375dd97d70fe0488ae7
Less <63160418+NLessW@users.noreply.github.com>
2024-04-08 19:16:35 +0900

    Create beginner.cpp

---
+++ a_cpp_solutions/0406_cpp/beginner.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    int sum = 0;
+    double answer = 0;
+    for(int i=0; i<arr.size(); i++){
+        sum += arr[i];
+    }
+    return (double)sum/arr.size();
+}
